b5fbbf4eaf60690daff31ac830af2f8c0953fbb3
gustavoarildo <gustavoarildo@gmail.com>
2020-05-04 15:47:44 -0300

    #2: manutencao de posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def preenche_titulo(titulo_da_pagina='')
+        titulo_padrao="Blog App"
+        if(titulo_da_pagina.empty?)
+            titulo_padrao
+        else
+            "#{titulo_da_pagina} | #{titulo_padrao}"
+        end
+    end
 end
