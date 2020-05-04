b50655e164c4884fcb912c104c85650bc6fb2053
gustavoarildo <gustavoarildo@gmail.com>
2020-05-04 13:38:47 -0300

    #1: manutencao de posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
 	def new
 		@post = Post.new
 	end
+
+	def index
+		@posts = Post.all
+	end
+
+	def edit
+		@posts = Post.find(params[:id ])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to @post
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to post_path
+	end
 end
